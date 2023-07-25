e1378ffe324faa40aa7ef91713f7ba1b190897fc
João Pedro Dias Caparroz <joao.caparroz@netpartners.com.br>
2023-07-25 15:56:30 -0300

    Parser junto com o pablito

---
+++ Aulas/aula_6/run.py
@@ -0,0 +1,21 @@
+## Condicionais if/else/elif
+
+x = 0
+
+if x == 2:
+    print('oi')
+
+elif x == 3:
+    
+    print('tabomvai')
+
+else: 
+    print('nao')
+    
+
+if x != 2:
+    print('nao eh dois')
+    
+if x:
+    print('tem valor')
+    
